e235ccabb7557ff96ae49733ba8e73e681cb583e
Đặng Quốc Huy <85658305+danieldev23@users.noreply.github.com>
2025-10-25 03:54:28 +0700

    updated

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,14 +5,7 @@ let collections: string[] = [];
 export async function connect(stringConnectUrl: string): Promise<void> {
   try {
     await databaseClient.connect(stringConnectUrl);
-    console.log("Connected to MongoDB");
-    const db = databaseClient.connection.db;
-    if (!db) {
-      throw new Error("Database connection is not initialized.");
-    }
-
-    const cols = await db.listCollections().toArray();
-    collections = cols.map((col) => col.name);
+    console.log("Connected to MongoDB!");
   } catch (error) {
     console.error("MongoDB connection error:", error);
     process.exit(1);
@@ -20,6 +13,3 @@ export async function connect(stringConnectUrl: string): Promise<void> {
 }
 
 // Hàm lấy danh sách collections
-export function getAllCollections(): string[] {
-  return collections;
-}
