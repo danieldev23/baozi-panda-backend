7931d78b392bc59abcdce07e141b91f89c74fd9c
Đặng Quốc Huy <85658305+danieldev23@users.noreply.github.com>
2025-10-26 01:18:20 +0700

    updated

--- src/routes/v1.routes.ts
+++ src/routes/v1.routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { vocabularyController } from "../utils/api.utils";
 // import userRoute from "./user.routes";
 import authRoute from "./auth.routes";
+import chatRoute from "./chat.routes";
 const router = Router();
 
 router.get("/v1/vocabulary", vocabularyController.getAll.bind(vocabularyController));
 router.get("/v1/vocabulary/search", vocabularyController.searchByQuery.bind(vocabularyController));
 router.get("/v1/vocabulary/tts", vocabularyController.text2Speech.bind(vocabularyController));
 router.use("/v1/auth", authRoute);
+router.use("/v1/chat", chatRoute);
 export default router;
